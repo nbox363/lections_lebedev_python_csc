dee41340d6e7e560fa66cdf536b5a1289219d429
nbox363 <overchukdo@gmail.com>
2020-09-11 10:44:04 +0300

    lectoin 7 - exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,8 +1,61 @@
-# объявление своего исключения
-class CSCEexception(Exception):
+try:
+    something_dangerous()
+except (ValueError, ArithmeticError):
+    pass
+except TypeError as e:  # 'e' существует только внутри блока except
     pass
 
 
-class TestFailure(CSCEexception):
+# ###################################################################################
+
+
+BaseException.__subclasses__()  # базовый класс для встроенных исключений
+# [<class 'Exception'>, <class 'GeneratorExit'>, <class 'SystemExit'>, <class 'KeyboardInterrupt'>]
+
+
+# ###################################################################################
+
+
+class CSCEexception(Exception):    # объявление своего исключения
+    pass
+
+
+class TestFailure(CSCEexception):  # наследуем от своего исключения
     def __str__(self):
         return 'lecture test failed'
+
+
+# ###################################################################################
+
+
+'''Исключение возникло в результате другого'''
+try:
+    {}['foobar']
+except KeyError as e:
+    raise RuntimeError('Oooops!') from e
+
+
+# ###################################################################################
+
+
+'''finaly'''
+try:
+    handle = open('example.txt', 'wt')
+    try:
+        do_something(handle)
+    finally:
+        handle.close()
+except IOError as e:
+    print(e, file=sys.stderr)
+
+
+# ###################################################################################
+
+
+'''else'''
+try:
+    handle = open('example.txt', 'wt')
+else:  # выполняется если исключения не было
+    report_success(handle)
+except IOError as e:
+    print(e, file=sys.stderr)
